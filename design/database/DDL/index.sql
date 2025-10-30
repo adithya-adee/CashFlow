2791df5556b4f0e25845604927cbbf8477518ffa
adithya-adee <adithya25905@gmail.com>
2025-10-31 01:19:07 +0530

    Design Docs finalize

--- design/database/DDL/index.sql
+++ design/database/DDL/index.sql
@@ -1,4 +1,4 @@
-
-CREATE INDEX account_filter ON CASHFLOW (account_id);
-CREATE INDEX category_filter ON CASHFLOW (category);
-CREATE INDEX cashflow_search ON CASHFLOW (account_id, category, txn_type);
+CREATE INDEX idx_cashflow_account_id ON CASHFLOW (account_id);
+CREATE INDEX idx_cashflow_category ON CASHFLOW (category);
+CREATE INDEX idx_cashflow_txn_type ON CASHFLOW (txn_type);
+CREATE INDEX idx_cashflow_composite_search ON CASHFLOW (account_id, category, txn_type);
